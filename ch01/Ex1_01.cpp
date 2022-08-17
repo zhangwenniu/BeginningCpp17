7d8fc00116662f1038480c51086365a999844e06
zhangwenniu <zhangwenniu@163.com>
2022-08-17 17:24:56 +0800

    第一个C++程序

---
+++ ch01/Ex1_01.cpp
@@ -0,0 +1,14 @@
+// Ex1_01.cpp
+// A complete C++ program
+
+#include <iostream>
+
+int main(){
+    int answer{42};  // Defines answer with value 42
+    std::cout << "The answer to life, the universe, and everything is "
+            << answer
+            << std::endl;
+
+    return 0;
+
+}
